d9c7ba1bafddd8d8610d8fadc65c2d45db93dfed
azheng249 <andy.zheng249@gmail.com>
2016-05-14 13:02:24 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,19 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should filter people by their favorite food', function(){
+    var people = [{
+      name: 'David',
+      favoriteFood: 'fish'
+    }, {
+      name: 'Ashley',
+      favoriteFood: 'chicken'
+    }];
+
+    var results = $filter('favoriteFood')(people, 'fish');
+
+    expect(results.length).toBe(1);
+    expect(results[0].favoriteFood).toBe('fish');
+  });
+
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,20 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should remove all vowels from a word', function(){
+    var word = 'television';
+
+    var result = $filter('removeAllVowels')(word);
+    
+    expect(result).toBe('tlvsn');
+  });
+
+  it('should ignore the cases when remove all vowels from a word', function(){
+    var word = 'ApplicAtiOn';
+
+    var result = $filter('removeAllVowels')(word);
+    
+    expect(result).toBe('pplctn');
+  });
+
 });
